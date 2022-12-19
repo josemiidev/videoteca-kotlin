0d5ff78f20065f2381aaf3c751690411fbf3c955
josemiguellorenzolara <josemiguel.lorenzolara1501@gmail.com>
2022-12-19 19:57:06 +0100

    Añadiendo diseño detalles peliculas

--- app/src/main/java/com/baileninformacion/videoteca/ui/DetailActivity.kt
+++ app/src/main/java/com/baileninformacion/videoteca/ui/DetailActivity.kt
@@ -1,11 +1,12 @@
-package com.baileninformacion.videoteca
+package com.baileninformacion.videoteca.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.baileninformacion.videoteca.R
 
-class MainActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_detail)
     }
 }
---
+++ app/src/main/java/com/baileninformacion/videoteca/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.baileninformacion.videoteca.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.baileninformacion.videoteca.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
