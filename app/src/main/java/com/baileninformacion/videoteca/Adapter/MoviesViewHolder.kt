4d4860a0450c9aa0a3ea035da6edbd62cd5189b2
josemiguellorenzolara <josemiguel.lorenzolara1501@gmail.com>
2022-12-22 23:15:17 +0100

    <E> Empezamos con las clases de muestreo

---
+++ app/src/main/java/com/baileninformacion/videoteca/Adapter/MoviesViewHolder.kt
@@ -0,0 +1,11 @@
+package com.baileninformacion.videoteca.Adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.baileninformacion.videoteca.model.Movie
+
+class MoviesViewHolder(view: View) :RecyclerView.ViewHolder(view){
+    fun bind(movie: Movie){
+        itemView.
+    }
+}
